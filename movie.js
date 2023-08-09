0f90a410e35e28ba6d200d9e4d2b9f6daa1b9763
nidhi <nidhi24sn@gmail.com>
2023-08-10 00:36:38 +0530

    movie posters are added

--- movie.js
+++ movie.js
@@ -1,31 +1,31 @@
-// Define the movie poster image URLs
+
 var moviePosters = [
     
     "img/pulpfiction.jpg",
     "img/fightclub.jpg",
     "img/theShawshankRedemption.jpg",
-    "img/theGodFather.jpg",
     "img/goodWillHunting.jpg",
     "img/spidermanAcrossTheSpiderVerse.jpeg",
     "img/spidermanIntoTheSpiderVerse.jpg",
-    "img/theGodFather2.jpg",
+    "img/theGodFather.jpg",
     "img/interstellar.jpg",
-    "img/inception.jpg",
-    "img/inception2.png",
-    "img/prideAndPrejudice2.jpg",
+    "img/inception.png",
+    
     "img/seven.jpg",
-    "img/memento.png"
+    "img/memento.png",
+    "img/everythingEverywhere.jpg",
+    "img/coco.jpg",
+    "img/superbad.webp",
+    "img/shutterIsland.jpg"
 
    
-    // Add more movie poster URLs here
 ];
 
-// Function to generate a random movie poster
+
 function generateMoviePoster() {
     var randomIndex = Math.floor(Math.random() * moviePosters.length);
     var randomMoviePoster = moviePosters[randomIndex];
     document.getElementById("moviePoster").src = randomMoviePoster;
 }
 
-// Event listener for the generate button
 document.getElementById("generateBtn").addEventListener("click", generateMoviePoster);
